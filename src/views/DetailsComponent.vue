e9f59556fb1fe572c3256e57b3c9902963f3dc11
chidiebere chukwudi <chidideveloper@gmail.com>
2023-12-02 15:43:02 +0100

    some ui fixes on the new branch

--- src/views/DetailsComponent.vue
+++ src/views/DetailsComponent.vue
@@ -8,31 +8,35 @@
         <div class="row mb-5" v-else>
             <div class="col-md-6 col-lg-4 mb-3">
                 <div class="card h-auto">
-                    <img class="card-img-top company-logo" width="200" height="200" :src="company.logo" alt="company logo" />
+                    <img class="card-img-top company-logo" width="200" height="200" :src="company.logo"
+                        alt="company logo" />
                     <div class="card-body">
                         <h5 class="card-title fst-normal"> {{ company.company }}</h5>
                         <p class="card-text">{{ company.about }}</p>
-                        <a :href="company.company_url" target="_blank" class="btn btn-outline-secondary">Visit Company website  </a>
+                        <a :href="company.company_url" target="_blank" class="btn btn-outline-secondary">Visit Company
+                            website </a>
                     </div>
                 </div>
             </div>
 
             <div class="col-md-6 col-lg-8">
-                <h4 class="pb-1 mb-4 text-start">Technology</h4>
-                <div class="card text-start mb-3 w-100">
-
-                    <div class="row">
-                        <TechnologyInfoCard title="Backend" :stacks="stack_be" :frameworks="be_framework" />
-                        <TechnologyInfoCard title="Frontend" :stacks="stack_fe" :frameworks="fe_framework" />
+                <h4 class="pb-1 mb-1 text-start">Technology</h4>
+                <div class="row p-2 justify-content-around">
+                    <div class="card text-start mb-3 col me-3 stackHeight" >
+                        <TechnologyInfoCardBe title="Backend" :stacks="stack_be" :frameworks="be_framework" />
                         <!-- <TechnologyInfoCard title="Devops" :stacks="company.devops" /> -->
                         <!-- <TechnologyInfoCard title="Database" :stacks="company.database_driver" /> -->
                     </div>
 
+                    <div class="card text-start mb-3 col stackHeight"  >
+                        <TechnologyInfoCardFe title="Frontend" :stacks="stack_fe" :frameworks="fe_framework" />
+
+                    </div>
                 </div>
                 <h4 class="mt-4 text-start">Human/Business</h4>
                 <div class="card text-start mb-3 w-100">
                     <div class="row">
-            
+
                         <HumanInfoCard title="CTO" :body="company.ceo" :link="company.ceo_contact" />
                         <HumanInfoCard title="CEO" :body="company.cto_name" :link="company.cto_contact" />
                         <HumanInfoCard title="Human Resource" :body="company.hr" :link="company.hr_contact" />
@@ -47,7 +51,8 @@
 <script>
 import getCompany from '@/composables/getCompany'
 import HumanInfoCard from '@/components/details/CompanyInfoCard/HumanInfoCard.vue'
-import TechnologyInfoCard from '@/components/details/CompanyInfoCard/TechnologyInfoCard.vue'
+import TechnologyInfoCardBe from '@/components/details/CompanyInfoCard/TechnologyInfoCardBe.vue'
+import TechnologyInfoCardFe from '@/components/details/CompanyInfoCard/TechnologyInfoCardFe.vue'
 import { ref } from 'vue'
 
 export default {
@@ -55,23 +60,24 @@ export default {
     name: 'DetailsComponent',
     components: {
         HumanInfoCard,
-        TechnologyInfoCard
+        TechnologyInfoCardBe,
+        TechnologyInfoCardFe
     },
 
     setup(props) {
 
         const fe_framework = ref([])
-        
-        const { 
-            company, 
-            isLoading, 
-            stack_be, 
+
+        const {
+            company,
+            isLoading,
+            stack_be,
             stack_fe,
             be_framework
         } = getCompany(props.id)
 
-        return { 
-            company, 
+        return {
+            company,
             isLoading,
             stack_be,
             stack_fe,
@@ -83,9 +89,11 @@ export default {
 </script>
 
 <style>
-
 .company-logo {
     object-fit: contain;
 }
 
+.stackHeight {
+    height: fit-content;
+}
 </style>
