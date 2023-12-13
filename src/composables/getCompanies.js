4e549504790e1c7e8f57ba76ecd22303c2854b14
Chidiebere Chukwudi <chidideveloper@gmail.com>
2023-12-13 02:01:26 +0100

    set dynamic environmental variables

--- src/composables/getCompanies.js
+++ src/composables/getCompanies.js
@@ -16,7 +16,7 @@ const useGetCompanies = () => {
 	const fetchData = async () => {
 
 		try {
-			const response = await axios.get("http://127.0.0.1:8000/api/company/stack/all");
+			const response = await axios.get(`${process.env.VUE_APP_ROOT_API}/api/company/stack/all`);
 			companies.value = response.data;
 		} catch (err) {
 			console.error("Error fetching data:", err.message);
