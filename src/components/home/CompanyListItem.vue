0eb3377dd37334c5ff69cdccafe6251c2f837010
Daniel Iheonu <64360587+danieliheonu@users.noreply.github.com>
2023-12-22 00:17:41 +0100

    chore: removed use of local storage

--- src/components/home/CompanyListItem.vue
+++ src/components/home/CompanyListItem.vue
@@ -1,82 +1,91 @@
 <template>
-    <div class="col-md-6 col-lg-4 mb-3">
-        <div class="card text-start" style="">
-            <div class="card-body my-0">
-                <div class="row justify-content-between" style="">
+	<div class="col-md-6 col-lg-4 mb-3">
+		<div class="card text-start" style="">
+			<div class="card-body my-0">
+				<div class="row justify-content-between" style="">
+					<div class="col-sm-9 col-8">
+						<h4 class="card-title">{{ company.company }}</h4>
+						<p class="card-text">
+							<span><b>Backend:</b></span
+							><span
+								class="ms-1"
+								v-for="stack_be in stacks_be"
+								:key="stack_be"
+							>
+								{{ stack_be }}</span
+							>
+							<br />
 
-                    <div class="col-sm-9 col-8">
-                        <h4 class="card-title">{{ company.company }}</h4>
-                        <p class="card-text">
-
-                            <span><b>Backend:</b></span><span class="ms-1" v-for="stack_be in stacks_be " :key="stack_be">
-                                {{ stack_be }}</span>
-                            <br />
-
-                            <span v-if="stacks_fe.length !== 0"><b> Frontend: </b> <span class="ms-1"
-                                    v-for="stack_fe in stacks_fe" :key="stack_fe">
-                                    {{
-                                        stack_fe
-                                    }} </span>
-                            </span>
-                            <span v-else></span>
-                        </p>
-                    </div>
-                    <div class="col-sm-3 col-4" style=" width: 100px; height: 100px;">
-                        <img class="img-fluid mt-3 mt-sm-auto" :src="company.logo" alt="Card image"
-                            style=" border-radius: 50%; vertical-align:bottom;" />
-                    </div>
-                </div>
-                <button @click="navigateToCompany(company.id)" class="btn btn-outline-secondary mt-3">Learn More</button>
-
-            </div>
-        </div>
-    </div>
+							<span v-if="stacks_fe.length !== 0"
+								><b> Frontend: </b>
+								<span
+									class="ms-1"
+									v-for="stack_fe in stacks_fe"
+									:key="stack_fe"
+								>
+									{{ stack_fe }}
+								</span>
+							</span>
+							<span v-else></span>
+						</p>
+					</div>
+					<div class="col-sm-3 col-4" style="width: 100px; height: 100px">
+						<img
+							class="img-fluid mt-3 mt-sm-auto"
+							:src="company.logo"
+							alt="Card image"
+							style="border-radius: 50%; vertical-align: bottom"
+						/>
+					</div>
+				</div>
+				<button
+					@click="navigateToCompany(company.id)"
+					class="btn btn-outline-secondary mt-3"
+				>
+					Learn More
+				</button>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
+	import getStacks from "@/composables/getStacks";
+	import { useStore } from "vuex";
+	import { useRouter } from "vue-router";
 
-import getStacks from '@/composables/getStacks'
-import { useStore } from 'vuex'
-import { useRouter } from 'vue-router';
+	export default {
+		props: ["company"],
+		name: "CompanyListItem",
+		components: {},
+		setup(props) {
+			const store = useStore();
+			const router = useRouter();
+			const stacks_be = getStacks(props.company.stack_be_plang);
+			const stacks_fe = getStacks(props.company.stack_fe_framework);
 
-export default {
-    props: ['company'],
-    name: 'CompanyListItem',
-    components: {
+			const navigateToCompany = (id) => {
+				// set company id in store
+				store.dispatch("setCompanyId", id);
 
-    },
-    setup(props) {
+				// changed route from company id to name
+				router.push({
+					name: "Details",
+					params: { name: props.company.company.toLowerCase().replace(/\s+/g, "-") },
+				});
+			};
 
-        const store = useStore()
-        const router = useRouter()
-        const stacks_be = getStacks(props.company.stack_be_plang)
-        const stacks_fe = getStacks(props.company.stack_fe_framework)
-
-        const navigateToCompany = (id) => {
-            // set company id in store
-            const storedId = localStorage.getItem('company');
-            if (storedId !== id) {
-                localStorage.setItem('company', JSON.stringify(id));
-                store.dispatch('setCompanyId', id)
-            }else{
-                store.dispatch('setCompanyId', JSON.parse(storedId))
-            }
-
-            // changed route from company id to name
-            router.push({ name: 'Details', params: { name: props.company.company.toLowerCase().replace(/\s+/g, '-') } })
-        }
-
-        return {
-            stacks_be,
-            stacks_fe,
-            navigateToCompany
-        }
-    },
-}
+			return {
+				stacks_be,
+				stacks_fe,
+				navigateToCompany,
+			};
+		},
+	};
 </script>
 
 <style>
-.card-title {
-    font-style: normal !important;
-}
+	.card-title {
+		font-style: normal !important;
+	}
 </style>
--- src/views/DetailsComponent.vue
+++ src/views/DetailsComponent.vue
@@ -1,44 +1,75 @@
 <template>
-    <div class="container-xxl flex-grow-1 container-p-y">
-        <div class="d-flex pb-2 back-btn" @click="goBack()">
-            <font-awesome-icon icon="fa-solid fa-arrow-left" class="d-flex justify-content-start" size="lg" />
-            <span class="text-start ms-2">Go Back</span>
-        </div>
-        <div class="d-flex justify-content-center mt-5" v-if="!isLoading">
-            <div class="spinner-border" role="status">
-                <span class="visually-hidden">Loading...</span>
-            </div>
-        </div>
-        <div class="row mb-5" v-else>
-            <div class="col-md-6 col-lg-4 mb-3">
-                <div class="card h-auto">
-                    <img class="card-img-top company-logo" width="200" height="200" :src="company.logo"
-                        alt="company logo" />
-                    <div class="card-body">
-                        <h5 class="card-title fst-normal"> {{ company.company }}</h5>
-                        <p class="card-text">{{ company.about }}</p>
-                        <a :href="company.company_url" target="_blank" class="btn btn-outline-secondary">Visit Company
-                            website </a>
-                    </div>
-                </div>
-            </div>
+	<div class="container-xxl flex-grow-1 container-p-y">
+		<div class="d-flex pb-2 back-btn" @click="goBack()">
+			<font-awesome-icon
+				icon="fa-solid fa-arrow-left"
+				class="d-flex justify-content-start"
+				size="lg"
+			/>
+			<span class="text-start ms-2">Go Back</span>
+		</div>
+		<div class="d-flex justify-content-center mt-5" v-if="!isLoading">
+			<div class="spinner-border" role="status">
+				<span class="visually-hidden">Loading...</span>
+			</div>
+		</div>
+		<div class="row mb-5" v-else>
+			<div class="col-md-6 col-lg-4 mb-3">
+				<div class="card h-auto">
+					<img
+						class="card-img-top company-logo"
+						width="200"
+						height="200"
+						:src="company.logo"
+						alt="company logo"
+					/>
+					<div class="card-body">
+						<h5 class="card-title fst-normal">{{ company.company }}</h5>
+						<p class="card-text">{{ company.about }}</p>
+						<a
+							:href="company.company_url"
+							target="_blank"
+							class="btn btn-outline-secondary"
+							>Visit Company website
+						</a>
+					</div>
+				</div>
+			</div>
 
-            <div class="col-md-6 col-lg-8">
-                <h4 class="pb-1 mb-1 text-start">Technology</h4>
-                <div class="row p-2 p-lg-0 gap-2">
-                    <div class="flex-fill card text-start p-0 mb-3 col-12 col-md-12 col-lg-5 stackHeight">
-                        <TechnologyInfoCardBe title="Backend" :stacks="stack_be" :frameworks="be_framework" />
-                        <!-- <TechnologyInfoCard title="Devops" :stacks="company.devops" /> -->
-                        <!-- <TechnologyInfoCard title="Database" :stacks="company.database_driver" /> -->
-                    </div>
-                    <div class="flex-fill card text-start p-0 mb-3 col-12 col-md-12 col-lg-5 stackHeight">
-                        <TechnologyInfoCardFe title="Frontend" :stacks="fe_stack" :frameworks="stack_fe" />
-                    </div>
-                    <div class="flex-fill card text-start p-0 mb-3 col-12 col-md-12 col-lg-5 stackHeight">
-                        <TechnologyInfoCardFe title="Mobile" :stacks="fe_stack" :frameworks="stack_fe" />
-                    </div>
-                </div>
-                <!-- <h4 class="mt-4 text-start">Human/Business</h4>
+			<div class="col-md-6 col-lg-8">
+				<h4 class="pb-1 mb-1 text-start">Technology</h4>
+				<div class="row p-2 p-lg-0 gap-2">
+					<div
+						class="flex-fill card text-start p-0 mb-3 col-12 col-md-12 col-lg-5 stackHeight"
+					>
+						<TechnologyInfoCardBe
+							title="Backend"
+							:stacks="stack_be"
+							:frameworks="be_framework"
+						/>
+						<!-- <TechnologyInfoCard title="Devops" :stacks="company.devops" /> -->
+						<!-- <TechnologyInfoCard title="Database" :stacks="company.database_driver" /> -->
+					</div>
+					<div
+						class="flex-fill card text-start p-0 mb-3 col-12 col-md-12 col-lg-5 stackHeight"
+					>
+						<TechnologyInfoCardFe
+							title="Frontend"
+							:stacks="fe_stack"
+							:frameworks="stack_fe"
+						/>
+					</div>
+					<div
+						class="flex-fill card text-start p-0 mb-3 col-12 col-md-12 col-lg-5 stackHeight"
+					>
+						<TechnologyInfoCardFe
+							title="Mobile"
+							:stacks="fe_stack"
+							:frameworks="stack_fe"
+						/>
+					</div>
+				</div>
+				<!-- <h4 class="mt-4 text-start">Human/Business</h4>
                 <div class="card text-start mb-2 w-100">
                     <div class="row">
 
@@ -48,68 +79,63 @@
                     </div>
 
                 </div> -->
-            </div>
-        </div>
-    </div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
-import getCompany from '@/composables/getCompany'
-// import HumanInfoCard from '@/components/details/CompanyInfoCard/HumanInfoCard.vue'
-import TechnologyInfoCardBe from '@/components/details/CompanyInfoCard/TechnologyInfoCardBe.vue'
-import TechnologyInfoCardFe from '@/components/details/CompanyInfoCard/TechnologyInfoCardFe.vue'
-import { ref } from 'vue'
-import { useStore } from 'vuex'
-import { useRouter } from 'vue-router'
+	import getCompany from "@/composables/getCompany";
+	// import HumanInfoCard from '@/components/details/CompanyInfoCard/HumanInfoCard.vue'
+	import TechnologyInfoCardBe from "@/components/details/CompanyInfoCard/TechnologyInfoCardBe.vue";
+	import TechnologyInfoCardFe from "@/components/details/CompanyInfoCard/TechnologyInfoCardFe.vue";
+	import { ref } from "vue";
+	import { useStore } from "vuex";
+	import { useRouter } from "vue-router";
 
-export default {
-    name: 'DetailsComponent',
-    components: {
-        // HumanInfoCard,
-        TechnologyInfoCardBe,
-        TechnologyInfoCardFe
-    },
+	export default {
+		name: "DetailsComponent",
+		components: {
+			// HumanInfoCard,
+			TechnologyInfoCardBe,
+			TechnologyInfoCardFe,
+		},
 
-    setup() {
+		setup() {
+			const store = useStore();
+			const router = useRouter();
+			const fe_stack = ref(["JavaScript"]);
+			const id = ref(store.state.companyId);
 
-        const store = useStore()
-        const router = useRouter()
-        const fe_stack = ref(["JavaScript"])
-        const id = ref(store.state.companyId)
+			const goBack = () => {
+				router.go(-1);
+			};
 
-        const goBack = () => {
-            router.go(-1)
-        }
+			const { company, isLoading, stack_be, stack_fe, be_framework } = getCompany(
+				id.value
+			);
 
-        const {
-            company,
-            isLoading,
-            stack_be,
-            stack_fe,
-            be_framework
-        } = getCompany(id.value || localStorage.getItem('company'))
-
-        return {
-            company,
-            isLoading,
-            stack_be,
-            stack_fe,
-            fe_stack,
-            be_framework,
-            goBack
-        }
-    }
-}
+			return {
+				company,
+				isLoading,
+				stack_be,
+				stack_fe,
+				fe_stack,
+				be_framework,
+				goBack,
+			};
+		},
+	};
 </script>
 
 <style>
-.company-logo {
-    object-fit: contain;
-}
-.stackHeight {
-    height: fit-content;
-}
-.back-btn{
-    cursor: pointer;
-}
+	.company-logo {
+		object-fit: contain;
+	}
+	.stackHeight {
+		height: fit-content;
+	}
+	.back-btn {
+		cursor: pointer;
+	}
 </style>
