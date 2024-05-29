c09fa21eb8c239b80772b47c0351efd9c345d64d
chidiebere chukwudi <chidideveloper@gmail.com>
2024-05-29 18:20:43 +0100

    keep tthis to replicate register

--- pages/community/login.vue
+++ pages/community/login.vue
@@ -1,27 +1,29 @@
 <template>
 	<div class="container-xxl flex-grow-1 container-p-y ">
-		<div class="row align-items-center justify-content-center" >
+		<div class="row align-items-center justify-content-center">
 			<div class="col-6 rounded bg-dark p-3">
-				<div class="mb-3">
-					<label for="formGroupExampleInput" class="form-label">Name</label>
-					<input type="text" class="form-control" id="formGroupExampleInput"
-						placeholder="Example input placeholder">
-				</div>
-				<div class="mb-3">
-					<label for="formGroupExampleInput" class="form-label">Email</label>
-					<input type="text" class="form-control" id="formGroupExampleInput"
-						placeholder="Example input placeholder">
-				</div>
-				<div class="mb-3">
-					<label for="formGroupExampleInput2" class="form-label">Password</label>
-					<input type="text" class="form-control" id="formGroupExampleInput2"
-						placeholder="Another input placeholder">
-				</div>
-				<div class="mb-3">
-					<label for="formGroupExampleInput2" class="form-label">Linkedin Url</label>
-					<input type="text" class="form-control" id="formGroupExampleInput2"
-						placeholder="Another input placeholder">
-				</div>
+				<form @submit.prevent="handleLogin">
+					<div class="mb-3">
+						<label for="formGroupExampleInput" class="form-label">Name</label>
+						<input type="text" class="form-control" id="formGroupExampleInput"
+							placeholder="Example input placeholder">
+					</div>
+					<div class="mb-3">
+						<label for="formGroupExampleInput" class="form-label">Email</label>
+						<input type="text" class="form-control" id="formGroupExampleInput"
+							placeholder="Example input placeholder">
+					</div>
+					<div class="mb-3">
+						<label for="formGroupExampleInput2" class="form-label">Password</label>
+						<input type="text" class="form-control" id="formGroupExampleInput2"
+							placeholder="Another input placeholder">
+					</div>
+					<div class="mb-3">
+						<label for="formGroupExampleInput2" class="form-label">Linkedin Url</label>
+						<input type="text" class="form-control" id="formGroupExampleInput2"
+							placeholder="Another input placeholder">
+					</div>
+				</form>
 			</div>
 		</div>
 	</div>
@@ -32,6 +34,16 @@ const email = ref("");
 const password = ref("");
 const error = ref("");
 
+const form = ref({
+	email: "jovialcore@gmail.com",
+	password: "jovialcore224",
+
+})
+
+function handleLogin (param) { 
+
+ }
+ 
 const login = () => {
 	if (email.value === "admin" && password.value === "password") {
 		// Redirect to dashboard or perform any other action upon successful login
