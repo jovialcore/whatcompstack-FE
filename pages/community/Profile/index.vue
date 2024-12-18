3487c108fd1190fa0640d3c63a50b2b5d00efa79
Johndiddles <johnadepelumi@gmail.com>
2024-12-18 09:52:40 +0100

    add edit user personal information and socials modals

--- pages/community/Profile/index.vue
+++ pages/community/Profile/index.vue
@@ -26,32 +26,11 @@
 
     <div class="bg-white py-4 px-5 rounded-3 mb-3">
       <div
-        class="d-flex justify-content-between align-items-center mb-3 border-bottom pb-3"
+        class="d-flex justify-content-between align-items-center mb-3 border-bottom pb-3 w-100"
       >
         <p class="fw-bold text-dark mb-0 fs-5">Personal Information</p>
-        <div>
-          <button
-            class="rounded-3 btn btn-outline-dark d-flex align-items-center gap-2"
-          >
-            <p class="mb-0">Edit</p>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              fill="currentColor"
-              class="bi bi-pencil-square"
-              viewBox="0 0 16 16"
-            >
-              <path
-                d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"
-              />
-              <path
-                fill-rule="evenodd"
-                d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"
-              />
-            </svg>
-          </button>
-        </div>
+
+        <ProfileEditPersonalInformationModal v-if="user" :user="user" />
       </div>
 
       <div class="d-flex flex-column gap-3">
@@ -85,48 +64,34 @@
         class="d-flex justify-content-between align-items-center mb-3 border-bottom pb-3"
       >
         <p class="fw-bold text-dark mb-0 fs-5">Socials</p>
-        <div>
-          <button
-            class="rounded-3 btn btn-outline-dark d-flex align-items-center gap-2"
-          >
-            <p class="mb-0">Edit</p>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              fill="currentColor"
-              class="bi bi-pencil-square"
-              viewBox="0 0 16 16"
-            >
-              <path
-                d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"
-              />
-              <path
-                fill-rule="evenodd"
-                d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"
-              />
-            </svg>
-          </button>
-        </div>
+        <ProfileEditSocialsModal v-if="user" :user="user" />
       </div>
 
       <div class="d-flex flex-column gap-3">
         <div class="row g-3">
           <div class="col-12 col-sm-6 col-md-4 col-lg-3">
             <p class="mb-0 text-secondary">Twitter</p>
-            <p class="mb-0 text-dark text-truncate">{{ user.twitterUrl }}</p>
+            <p class="mb-0 text-dark text-truncate">
+              {{ user.socials.twitterUrl }}
+            </p>
           </div>
           <div class="col-12 col-sm-6 col-md-4 col-lg-3">
             <p class="mb-0 text-secondary">LinkedIn</p>
-            <p class="mb-0 text-dark text-truncate">{{ user.linkedinUrl }}</p>
+            <p class="mb-0 text-dark text-truncate">
+              {{ user.socials.linkedinUrl }}
+            </p>
           </div>
           <div class="col-12 col-sm-6 col-md-4 col-lg-3">
             <p class="mb-0 text-secondary">Github</p>
-            <p class="mb-0 text-dark text-truncate">{{ user.githubUrl }}</p>
+            <p class="mb-0 text-dark text-truncate">
+              {{ user.socials.githubUrl }}
+            </p>
           </div>
           <div class="col-12 col-sm-6 col-md-4 col-lg-3">
             <p class="mb-0 text-secondary">Behance</p>
-            <p class="mb-0 text-dark text-truncate">{{ user.behanceUrl }}</p>
+            <p class="mb-0 text-dark text-truncate">
+              {{ user.socials.behanceUrl }}
+            </p>
           </div>
         </div>
       </div>
@@ -145,10 +110,12 @@ const user = {
   id: 1,
   location: "Lagos, Nigeria",
   avatar,
-  twitterUrl: "https://x.com/alexandraSeraph",
-  githubUrl: "https://github.com/alexandraSeraph",
-  linkedinUrl: "https://linkedin.com/in/alexandraSeraph",
-  behanceUrl: "",
+  socials: {
+    twitterUrl: "https://x.com/alexandraSeraph",
+    githubUrl: "https://github.com/alexandraSeraph",
+    linkedinUrl: "https://linkedin.com/in/alexandraSeraph",
+    behanceUrl: "",
+  },
 };
 </script>
 
