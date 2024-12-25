9bc63ba0300e33495c02a71554ea72f07e94d02e
Johndiddles <johnadepelumi@gmail.com>
2024-12-25 16:27:10 +0100

    add multi-step form for new company and company stack

---
+++ components/form/InputField.vue
@@ -0,0 +1,54 @@
+<template>
+  <label
+    class="form-label fw-semibold text-secondary text-uppercase mb-1"
+    :for="id"
+    >{{ label }}</label
+  >
+  <input
+    :type="type"
+    class="form-control py-2 text-dark"
+    style="border-radius: 10px; font-size: 0.8rem"
+    :id="id"
+    :value="inputValue"
+  />
+</template>
+<script setup>
+const props = defineProps({
+  id: {
+    type: String,
+    default: "",
+  },
+  value: {
+    type: [String, Number],
+    default: "",
+  },
+  label: {
+    type: String,
+    default: "",
+  },
+  type: {
+    type: String,
+    default: "text",
+    validator: (value) =>
+      ["text", "email", "password", "number", "tel"].includes(value),
+  },
+  placeholder: {
+    type: String,
+    default: "",
+  },
+  required: {
+    type: Boolean,
+    default: false,
+  },
+  disabled: {
+    type: Boolean,
+    default: false,
+  },
+  error: {
+    type: String,
+    default: "",
+  },
+});
+
+const inputValue = ref(props.value);
+</script>
